f99393786189023230cdd91e802ac879050ac25e
cjl12377 <69098684+cjl12377@users.noreply.github.com>
2021-03-14 19:25:59 +0800

    Sends via Telegram bot instead of SMS
    Wanted SMS instead of SMS but hey your choice. Twilio has limited free credits, telegram doesn't.
    I hardcorded the Telegram chat_id after starting a chat with the bot at @stock_news_alertbot so it only sends to me and not everyone. Just don't close the chat if not you'll have to update your env token whereever you're running it

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
 STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
 NEWS_API = os.environ.get("NEWS_API")
+TELE_API_KEY = os.environ.get("TELE_API_KEY")
+CHAT_ID = os.environ.get("CHAT_ID")
+
+
 
 def get_stock_alert():
     STOCK_ENDPOINT = "https://www.alphavantage.co/query"
@@ -31,9 +35,9 @@ def get_stock_alert():
     if day_changes <0:
         up_down = "🔻"
 
-    if (abs(day_changes)/opening_price)*100 > 5:
+    if (abs(day_changes)/opening_price)*100 > 1.0:
         get_news(day_changes, up_down)
-        
+
 
 def get_news(day_changes, up_down):
     NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
@@ -45,27 +49,39 @@ def get_news(day_changes, up_down):
     response = requests.get(url=NEWS_ENDPOINT, params=NEWS_PARAMS)
     response.raise_for_status()
     articles = response.json()['articles']
-    formatted_articles = [f"\n{STOCK}: {up_down}{round(abs(day_changes),2)}%\nHeadline: {article['title']}\n\nText: {article['description']}\nURL: {article['url']}" for article in articles]
+    formatted_articles = [f"\n{STOCK}: {up_down}{round(abs(day_changes),2)}% \n\nHeadline: {article['title']}\n\nText: {article['description']}\nURL: {article['url']}" for article in articles]
 
-    send_sms(formatted_articles)
-                          
-                          
-def send_sms(formatted_articles):
+    send_telegram(formatted_articles=formatted_articles)
+
+# def send_sms(formatted_articles):
+#     """"sends 1 message for every article. Num of articles pulled can be changed at pageSize in NEWS_PARAMS"""
+#     messages = formatted_articles
+#     proxy_client = TwilioHttpClient()
+#     proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+#
+#     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, http_client=proxy_client)
+#
+#     for i in messages:
+#         message = client.messages.create(
+#             body=i,
+#             from_="+18329812771",
+#             to="+6590211777"
+#         )
+#         print(message.sid)
+
+
+def send_telegram(formatted_articles):
     """"sends 1 message for every article. Num of articles pulled can be changed at pageSize in NEWS_PARAMS"""
     messages = formatted_articles
-    proxy_client = TwilioHttpClient()
-    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, http_client=proxy_client)
-
+    telegram_endpoint = f"https://api.telegram.org/bot{TELE_API_KEY}/sendMessage"
     for i in messages:
-        message = client.messages.create(
-            body=i,
-            from_="+18329812771",
-            to="+6590211777"
-        )
-        print(message.sid)
+        params = {
+            "chat_id": CHAT_ID,
+            "text": i
+        }
+        response = requests.post(params=params, url=telegram_endpoint)
+        response.raise_for_status()
 
-                          
+
 get_stock_alert()
-
